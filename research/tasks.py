d6da5deb8144f6e7da9239bab730d2bde32000e0
yohaboy <yohannesgizaw777@gmail.com>
2025-07-09 08:40:42 +0300

    parallel_processinggg

--- research/tasks.py
+++ research/tasks.py
@@ -1,18 +1,29 @@
-from celery import shared_task
+from celery import shared_task, group
 from datetime import datetime, timedelta
 import csv
 import io
 from research.models import Author, ResearchGroup
+from .views import robust_scrape_staff_ids, fetch_and_store_publications_for_author
+
+@shared_task
+def fetch_publications_for_author_task(author_id, since_date_str):
+    from research.models import Author
+    from .views import fetch_and_store_publications_for_author
+    author = Author.objects.get(id=author_id)
+    since_date = datetime.strptime(since_date_str, '%Y-%m-%d').date()
+    fetch_and_store_publications_for_author(author, since_date)
 
 @shared_task
 def fetch_publications_task(since_days=365):
-    from .views import fetch_and_store_all_publications
+    from research.models import Author
     since_date = datetime(2024, 1, 1).date()
-    fetch_and_store_all_publications(since_date)
+    # since_date = datetime.now().date() - timedelta(days=since_days)
+    author_ids = list(Author.objects.values_list('id', flat=True))
+    job = group(fetch_publications_for_author_task.s(author_id, since_date.strftime('%Y-%m-%d')) for author_id in author_ids)
+    result = job.apply_async()
 
 @shared_task
 def process_csv_upload(data):
-    from .views import robust_scrape_staff_ids
     io_string = io.StringIO(data)
     reader = csv.DictReader(io_string)
     for row in reader:
